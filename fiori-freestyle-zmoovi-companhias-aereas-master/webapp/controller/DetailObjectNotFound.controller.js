afeb3cc5877119c4e6b1a8ee9b61e2927bd97d35
theuzs <119632707+theuzs@users.noreply.github.com>
2024-04-23 12:09:35 -0300

    Add files via upload

---
+++ fiori-freestyle-zmoovi-companhias-aereas-master/webapp/controller/DetailObjectNotFound.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+    "./BaseController"
+], function (BaseController) {
+    "use strict";
+
+    return BaseController.extend("moovi.zpssilvaapp.controller.DetailObjectNotFound", {});
+});
---
+++ fiori-freestyle-zmoovi-companhias-aereas-master/webapp/test/integration/pages/Common.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+	"sap/ui/test/Opa5"
+], function(Opa5) {
+	"use strict";
+
+	return Opa5.extend("moovi.zpssilvaapp.test.integration.pages.Common", {
+
+	});
+
+});
